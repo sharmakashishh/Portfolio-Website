88234d217cabda9e72df4d2d2929863433b1ebc5
Kashish Sharma <kashishsharma0214@gmail.com>
2025-05-26 19:30:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,26 @@ function addRecommendation() {
       document.getElementById('popup').style.visibility = 'hidden'
     }
   }
+
+  function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+
+  const sections = document.querySelectorAll("nav, section, .skill, .project-card");
+  sections.forEach(sec => sec.classList.toggle("dark-mode"));
+}
+
+const text = "Hi, I'm Kashish Sharma!";
+let index = 0;
+
+function typeText() {
+  if (index < text.length) {
+    document.getElementById("typed-text").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeText, 100); // speed of typing
+  }
+}
+
+window.onload = () => {
+  typeText(); // Start typing on page load
+};
+
